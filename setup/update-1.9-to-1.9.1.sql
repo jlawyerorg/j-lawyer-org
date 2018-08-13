0f964c71e565bf25af014f7b7b3828271c3c0b97
j-dimension <info@j-lawyer.org>
2018-08-13 21:57:02 +0200

    added campaigns persistence and services

--- setup/update-1.9-to-1.9.1.sql
+++ setup/update-1.9-to-1.9.1.sql
@@ -15,4 +15,22 @@ insert into ServerSettingsBean(settingKey, settingValue) values('jlawyer.server.
 
 delete from ServerSettingsBean where settingKey='jlawyer.server.backup.maxbackups';
 
+CREATE TABLE campaign (
+`id` VARCHAR(50) BINARY NOT NULL, 
+`name` VARCHAR(250) BINARY, 
+CONSTRAINT `pk_campaign` PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE campaign_addresses (
+`id` VARCHAR(50) BINARY NOT NULL, 
+`campaignKey` VARCHAR(50) BINARY, 
+`addressKey` VARCHAR(250) BINARY, 
+CONSTRAINT `pk_campaignaddresses` PRIMARY KEY (`id`),
+FOREIGN KEY (campaignKey) REFERENCES campaign(id) ON DELETE CASCADE,
+FOREIGN KEY (addressKey) REFERENCES AddressBean(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+alter table campaign_addresses add index `IDX_CAMPAIGNADDRESSES_CAMPAIGNKEY` (campaignKey);
+alter table campaign_addresses add index `IDX_CAMPAIGNADDRESSES_ADDRESSKEY` (addressKey);
+
 commit;
