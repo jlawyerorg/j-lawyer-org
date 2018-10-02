98528b0ecf41c8d4aa582d35e12a91d07051f076
j-dimension <info@j-lawyer.org>
2018-10-02 22:16:51 +0200

    increased archivefileaddressesbean.contact to 250 chars

--- setup/update-1.9-to-1.9.1.sql
+++ setup/update-1.9-to-1.9.1.sql
@@ -2,7 +2,7 @@ use jlawyerdb;
 
 alter table ArchiveFileAddressesBean add `reference` VARCHAR(50) BINARY;
 # Sachbearbeiter
-alter table ArchiveFileAddressesBean add `contact` VARCHAR(50) BINARY;
+alter table ArchiveFileAddressesBean add `contact` VARCHAR(250) BINARY;
 alter table ArchiveFileAddressesBean add `custom1` VARCHAR(250) BINARY;
 alter table ArchiveFileAddressesBean add `custom2` VARCHAR(250) BINARY;
 alter table ArchiveFileAddressesBean add `custom3` VARCHAR(250) BINARY;
