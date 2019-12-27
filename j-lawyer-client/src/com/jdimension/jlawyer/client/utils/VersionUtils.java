dbd63599d84eb8eb9c7b79f2f20671eac8e8d254
j-dimension <info@j-lawyer.org>
2019-12-27 22:15:29 +0100

    Prep for 1.11.0.3

--- j-lawyer-client/src/com/jdimension/jlawyer/client/utils/VersionUtils.java
+++ j-lawyer-client/src/com/jdimension/jlawyer/client/utils/VersionUtils.java
@@ -687,7 +687,7 @@ public class VersionUtils {
     }
 
     public static String getBuild() {
-        return "2";
+        return "3";
     }
     
     public static boolean isVersionGreater(String referenceVersion, String compareToVersion) {
