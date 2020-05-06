70237fca3eeb694df589cdd3dc0d98842b12c622
j-dimension <info@j-lawyer.org>
2020-05-06 23:18:26 +0200

    1.12.0.6

--- j-lawyer-client/src/com/jdimension/jlawyer/client/utils/VersionUtils.java
+++ j-lawyer-client/src/com/jdimension/jlawyer/client/utils/VersionUtils.java
@@ -687,7 +687,7 @@ public class VersionUtils {
     }
 
     public static String getBuild() {
-        return "5";
+        return "6";
     }
     
     public static boolean isVersionGreater(String referenceVersion, String compareToVersion) {
