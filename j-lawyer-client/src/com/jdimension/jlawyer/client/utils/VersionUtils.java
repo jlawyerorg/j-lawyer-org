5ece682e1a015726e91594fbd108f129a986a7b8
j-dimension <info@j-lawyer.org>
2020-05-15 21:38:42 +0200

    version bump

--- j-lawyer-client/src/com/jdimension/jlawyer/client/utils/VersionUtils.java
+++ j-lawyer-client/src/com/jdimension/jlawyer/client/utils/VersionUtils.java
@@ -687,7 +687,7 @@ public class VersionUtils {
     }
 
     public static String getBuild() {
-        return "7";
+        return "8";
     }
     
     public static boolean isVersionGreater(String referenceVersion, String compareToVersion) {
