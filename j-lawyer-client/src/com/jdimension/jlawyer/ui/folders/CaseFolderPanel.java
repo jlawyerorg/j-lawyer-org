6b72e108baea7b8eac6945d07638d31620f2db20
j-dimension <info@j-lawyer.org>
2021-04-03 21:13:51 +0200

    removed irrelevant import

--- j-lawyer-client/src/com/jdimension/jlawyer/ui/folders/CaseFolderPanel.java
+++ j-lawyer-client/src/com/jdimension/jlawyer/ui/folders/CaseFolderPanel.java
@@ -679,7 +679,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
