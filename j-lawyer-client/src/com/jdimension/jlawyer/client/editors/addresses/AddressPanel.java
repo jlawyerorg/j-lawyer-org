a0ee96cdc62f3afac274952897a2dfd894120ec2
j-dimension <info@j-lawyer.org>
2023-03-09 22:13:52 +0100

    dummy change

--- j-lawyer-client/src/com/jdimension/jlawyer/client/editors/addresses/AddressPanel.java
+++ j-lawyer-client/src/com/jdimension/jlawyer/client/editors/addresses/AddressPanel.java
@@ -811,7 +811,6 @@ public class AddressPanel extends javax.swing.JPanel implements BeaLoginCallback
             @Override
             public void actionPerformed(ActionEvent ae) {
                 cmdChooseBankActionPerformed(null);
-
             }
         });
 
