c5c48cd7132dcc09831beca81b2280dbcd375c87
j-dimension <info@j-lawyer.org>
2023-08-23 23:46:15 +0200

    multi-line position description. close #2027

--- j-lawyer-client/src/com/jdimension/jlawyer/client/configuration/TimesheetPositionTemplatesSetupDialog.java
+++ j-lawyer-client/src/com/jdimension/jlawyer/client/configuration/TimesheetPositionTemplatesSetupDialog.java
@@ -728,7 +728,7 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
 
     private void resetDetails() {
         this.txtName.setText("");
-        this.txtDescription.setText("");
+        this.taDescription.setText("");
         this.txtUnitPrice.setText("0");
         this.txtTaxRate.setText("19");
     }
@@ -752,13 +752,14 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
         txtName = new javax.swing.JTextField();
         cmdSave = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
-        txtDescription = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         lblError = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
         txtUnitPrice = new javax.swing.JFormattedTextField();
         jLabel4 = new javax.swing.JLabel();
         txtTaxRate = new javax.swing.JFormattedTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        taDescription = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Zeiterfassungspositionen");
@@ -877,6 +878,10 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
 
         txtTaxRate.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("#0.00"))));
 
+        taDescription.setColumns(20);
+        taDescription.setRows(5);
+        jScrollPane2.setViewportView(taDescription);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -902,14 +907,15 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
                             .addComponent(jLabel4))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtTaxRate)
-                            .addComponent(txtDescription)
                             .addComponent(txtName)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(lblError, javax.swing.GroupLayout.DEFAULT_SIZE, 5, Short.MAX_VALUE)
                                 .addGap(258, 258, 258))
+                            .addComponent(jScrollPane2)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(txtUnitPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(txtTaxRate, javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtUnitPrice, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE))
                                 .addGap(0, 0, Short.MAX_VALUE)))))
                 .addContainerGap())
         );
@@ -926,10 +932,10 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
                             .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel1))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(txtDescription, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel14)
                             .addComponent(txtUnitPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -937,9 +943,9 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel4)
                             .addComponent(txtTaxRate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(341, 341, 341)
+                        .addGap(286, 286, 286)
                         .addComponent(lblError)
-                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGap(0, 2, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(cmdClose)
                             .addComponent(cmdSave))))
@@ -1036,7 +1042,7 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
             
             TimesheetPositionTemplate tpl = (TimesheetPositionTemplate) this.tblTemplates.getValueAt(row, 0);
             tpl.setName(this.txtName.getText());
-            tpl.setDescription(this.txtDescription.getText());
+            tpl.setDescription(this.taDescription.getText());
             tpl.setTaxRate(taxRate.floatValue());
             tpl.setUnitPrice(unitPrice.floatValue());
             
@@ -1094,7 +1100,7 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
     private void updateUI(TimesheetPositionTemplate tpl) {
         
         this.txtName.setText(tpl.getName());
-        this.txtDescription.setText(tpl.getDescription());
+        this.taDescription.setText(tpl.getDescription());
         this.txtTaxRate.setValue(tpl.getTaxRate());
         this.txtUnitPrice.setValue(tpl.getUnitPrice());
 
@@ -1164,9 +1170,10 @@ public class TimesheetPositionTemplatesSetupDialog extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblError;
+    private javax.swing.JTextArea taDescription;
     private javax.swing.JTable tblTemplates;
-    private javax.swing.JTextField txtDescription;
     private javax.swing.JTextField txtName;
     private javax.swing.JFormattedTextField txtTaxRate;
     private javax.swing.JFormattedTextField txtUnitPrice;
