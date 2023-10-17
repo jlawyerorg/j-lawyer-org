2a1164657736f91302da842ca835790aac152954
j-dimension <info@j-lawyer.org>
2023-10-17 17:06:39 +0200

    continuing in branch. issue #54.

--- j-lawyer-fax/test/org/jlawyer/tests/EpostClientTest.java
+++ j-lawyer-fax/test/org/jlawyer/tests/EpostClientTest.java
@@ -681,6 +681,7 @@ public class EpostClientTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // replace with real values
         String vendorId = "vendor";
         String customerId = "customer";
         
