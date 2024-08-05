9fab0159b75c66fb4d2416a5759fefffa8f3b2e2
j-dimension <info@j-lawyer.org>
2024-08-05 22:16:05 +0200

    switched to using UI builder event handler

--- j-lawyer-client/src/com/jdimension/jlawyer/client/mail/ViewEmailDialog.java
+++ j-lawyer-client/src/com/jdimension/jlawyer/client/mail/ViewEmailDialog.java
@@ -720,12 +720,6 @@ public class ViewEmailDialog extends javax.swing.JDialog {
         this.odoc = odoc;
 
         ComponentUtils.restoreDialogSize(this);
-        
-        addComponentListener(new java.awt.event.ComponentAdapter() {
-            public void componentShown(java.awt.event.ComponentEvent evt) {
-                formComponentShown(evt);
-            }
-        });
 
     }
 
@@ -761,6 +755,9 @@ public class ViewEmailDialog extends javax.swing.JDialog {
             public void componentResized(java.awt.event.ComponentEvent evt) {
                 formComponentResized(evt);
             }
+            public void componentShown(java.awt.event.ComponentEvent evt) {
+                formComponentShown(evt);
+            }
         });
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
@@ -768,7 +765,6 @@ public class ViewEmailDialog extends javax.swing.JDialog {
             }
         });
 
-        jToolBar1.setFloatable(false);
         jToolBar1.setRollover(true);
 
         cmdReply.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons32/mail_reply.png"))); // NOI18N
@@ -1105,10 +1101,10 @@ public class ViewEmailDialog extends javax.swing.JDialog {
             this.odoc.setClosed(true);
         }
     }//GEN-LAST:event_formWindowClosing
-    
-    private void formComponentShown(java.awt.event.ComponentEvent evt) {                                    
+
+    private void formComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_formComponentShown
         FrameUtils.centerDialogOnParentMonitor(this, this.getOwner().getLocation());
-    } 
+    }//GEN-LAST:event_formComponentShown
     
     /**
      * @param args the command line arguments
