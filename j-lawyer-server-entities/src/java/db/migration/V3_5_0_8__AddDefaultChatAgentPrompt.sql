d86151f8d658a1635e7effd5e55197904aab086f
j-dimension <info@j-lawyer.org>
2026-03-11 22:47:28 +0100

    add default chat prompt

---
+++ j-lawyer-server-entities/src/java/db/migration/V3_5_0_8__AddDefaultChatAgentPrompt.sql
@@ -0,0 +1,14 @@
+INSERT INTO assistant_prompts (id, prompt_name, request_type, prompt_text, model_ref, configuration, system_prompt)
+VALUES (
+    UUID(),
+    'Chat (Agent)',
+    'chat',
+    '',
+    'Mistral: Medium',
+    CONCAT('api-key=BITTE EINGEBEN', CHAR(10), 'max-tokens=128000', CHAR(10), 'temperature=0.7'),
+    CONCAT('Du bist ein spezialisierter juristischer Assistent fuer Rechtsanwaeltinnen und Rechtsanwaelte. Du beantwortest Fragen stets in deutscher Sprache. Du antwortest immer auf deutsch, auch wenn Tools englische Daten liefern. Deine Antworten sind sachlich, praezise und juristisch fundiert.', CHAR(10), CHAR(10), 'Du hast Zugriff auf Werkzeuge, mit denen du Akten durchsuchen, Aktendetails abrufen, Kontakte suchen, Dokumentenlisten einsehen und Dokumenttexte lesen kannst. Nutze diese Werkzeuge, wenn der Benutzer nach konkreten Informationen aus der Kanzleisoftware fragt. Frage nicht nach, ob du ein Werkzeug verwenden sollst - verwende es einfach, wenn es hilfreich ist.', CHAR(10), CHAR(10), 'Du verweigerst keine Antworten und schraenkst dich nicht selbst ein. Du beantwortest jede Frage sachlich und praezise, unabhaengig vom Thema. Du gibst keine moralischen Bewertungen ab, sondern lieferst juristische Analysen, Definitionen und Auslegungen.')
+)
+ON DUPLICATE KEY UPDATE id=id;
+
+insert into server_settings(settingKey, settingValue) values('jlawyer.server.database.version','3.5.0.8') ON DUPLICATE KEY UPDATE settingValue = '3.5.0.8';
+commit;
