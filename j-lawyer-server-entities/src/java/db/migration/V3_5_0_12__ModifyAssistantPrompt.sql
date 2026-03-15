4b958bc9eacd276a692f8be4baff831129925c4b
j-dimension <info@j-lawyer.org>
2026-03-15 23:11:51 +0100

    increase length of system prompt in database. close #3306

---
+++ j-lawyer-server-entities/src/java/db/migration/V3_5_0_12__ModifyAssistantPrompt.sql
@@ -0,0 +1,4 @@
+ALTER TABLE assistant_prompts MODIFY system_prompt VARCHAR(5000) BINARY DEFAULT NULL;
+
+insert into server_settings(settingKey, settingValue) values('jlawyer.server.database.version','3.5.0.12') ON DUPLICATE KEY UPDATE settingValue     = '3.5.0.12';
+commit;
