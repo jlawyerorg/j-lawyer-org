1b1dc5a8bb57661e43884c1e24a37ad8b6b0fd9f
j-dimension <info@j-lawyer.org>
2026-04-09 13:11:52 +0200

    repaint after validation

--- j-lawyer-client/src/com/jdimension/jlawyer/client/editors/finance/ManagePaymentsFrame.java
+++ j-lawyer-client/src/com/jdimension/jlawyer/client/editors/finance/ManagePaymentsFrame.java
@@ -1035,6 +1035,9 @@ public class ManagePaymentsFrame extends javax.swing.JFrame {
 
         }
 
+        this.pnlPayments.revalidate();
+        this.pnlPayments.repaint();
+
         return allValid;
     }
 
